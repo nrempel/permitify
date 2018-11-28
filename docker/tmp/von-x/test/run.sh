5ed6baad0f5143743e20a781b80c14f159e8e551
Nicholas Rempel <nbrempel@gmail.com>
2018-11-28 15:20:24 -0800

    Remove tmp file
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- docker/tmp/von-x/test/run.sh
+++
@@ -1,5 +0,0 @@
-DIR=`dirname $0`
-docker build -t vonx-test -f "$DIR/Dockerfile" "$DIR/.." || exit 1
-docker run --rm  -p ${PORT:-5000}:5000 -v vonx-wallet:/home/indy/.indy_client/wallet \
-  -e "LOG_LEVEL=${LOG_LEVEL:-INFO}" \
-  -ti vonx-test python -m test."${TEST:-testSequence}"
